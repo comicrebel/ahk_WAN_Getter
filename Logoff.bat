d1a1990f66a256886e0841e994fd8466c946c57b
comicrebel <tim@tmr2.com>
2016-10-18 14:28:49 -0600

    Add files via upload

---
+++ Logoff.bat
@@ -0,0 +1,7 @@
+START C:\Windows\System32\tscon.exe 0 /dest:console
+START C:\Windows\System32\tscon.exe 1 /dest:console
+START C:\Windows\System32\tscon.exe 2 /dest:console
+START C:\Windows\System32\tscon.exe 2 /dest:console
+START C:\Windows\System32\tscon.exe 3 /dest:console
+START C:\Windows\System32\tscon.exe 4 /dest:console
+START C:\Windows\System32\tscon.exe 5 /dest:console
